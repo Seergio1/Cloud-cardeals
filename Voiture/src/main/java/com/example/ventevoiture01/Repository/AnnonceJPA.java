4997c0e118823a9881a3617fa4133d14a47fa57f
Steel Soul <120133470+P15B-Y32-ETU1951@users.noreply.github.com>
2024-02-10 02:35:58 +0300

    Update AnnonceJPA.java

--- Voiture/src/main/java/com/example/ventevoiture01/Repository/AnnonceJPA.java
+++ Voiture/src/main/java/com/example/ventevoiture01/Repository/AnnonceJPA.java
@@ -6,6 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.example.ventevoiture01.Models.Annonce;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,5 +20,12 @@ public interface AnnonceJPA extends JpaRepository<Annonce, Integer> {
   @Transactional
   @Query("UPDATE Annonce a SET a.etat_annonce = '1' WHERE a.id_annonce = :id")
   void valider(@Param("id") int id);
+
+  @Modifying
+  @Transactional
+  @Query("UPDATE Annonce a SET a.validation_annonce = :date WHERE a.id_annonce = :id")
+  void date_validation(@Param("id") int id, @Param("date") LocalDateTime date);
+
+  Page<Annonce> findAll(Pageable pageable);
 
 }
