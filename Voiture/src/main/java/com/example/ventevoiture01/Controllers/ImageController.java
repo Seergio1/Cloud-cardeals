76a37e81ed38f2ee820a0f3a3354ce380e4e368b
Steel Soul <120133470+P15B-Y32-ETU1951@users.noreply.github.com>
2024-02-10 07:56:50 +0300

    Update ImageController.java

--- Voiture/src/main/java/com/example/ventevoiture01/Controllers/ImageController.java
+++ Voiture/src/main/java/com/example/ventevoiture01/Controllers/ImageController.java
@@ -4,9 +4,11 @@ import com.example.ventevoiture01.Models.Image;
 import com.example.ventevoiture01.Services.ImageService;
 import com.example.ventevoiture01.Utils.FileHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
